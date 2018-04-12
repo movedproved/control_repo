bcbd3b95b9d6ff8f1d5940cf5dfa584a258747cb
movedproved <13520136@gm.uit.edu.vn>
2018-04-12 15:20:02 +0700

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -7,5 +7,13 @@ node default {
 } 
 
 node 'puppet' {
-  include role::master_server,
+  include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
 }
